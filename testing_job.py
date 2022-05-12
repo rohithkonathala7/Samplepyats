ca8322f2c8862cbfddfb9e8366e111c15c470f36
rohithkonathala7 <104819816+rohithkonathala7@users.noreply.github.com>
2022-05-12 15:51:21 +0530

    This is a job file

--- testing_job.py
+++ testing_job.py
@@ -2,11 +2,6 @@
 testing_job.py
 
 '''
-# see https://pubhub.devnetcloud.com/media/pyats/docs/easypy/jobfile.html
-# for how job files work
-
-# optional author information
-# (update below with your contact information if needed)
 __author__ = 'Cisco Systems Inc.'
 __copyright__ = 'Copyright (c) 2019, Cisco Systems Inc.'
 __contact__ = ['pyats-support-ext@cisco.com']
@@ -14,13 +9,18 @@ __credits__ = ['list', 'of', 'credit']
 __version__ = 1.0
 
 import os
-from pyats.easypy import run
+from genie.harness.main import grun
 
-# compute the script path from this location
 SCRIPT_PATH = os.path.dirname(__file__)
 
 def main(runtime):
     '''job file entrypoint'''
+    
+    grun(
+    	pdb = False,
+    	trigger_datafile = "~/pyats/project/testing/testing_data.yaml",
+    	trigger_uids = ["InitializeTestbed"]
+    )
     
     # run script
     run(testscript= os.path.join(SCRIPT_PATH, 
